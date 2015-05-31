55e3e01de521cabab1da0c2c73a2bbce89ddfd01
RJ Robinson <robertj.robinson@gmail.com>
2015-05-31 12:10:27 -0400

    added confirmable to email hosts

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 20150527173513) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "user_name",              default: "", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -28,10 +30,15 @@ ActiveRecord::Schema.define(version: 20150527173513) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
--- spec/feature_spec/user_can_sign_in_out_and_delete_account_spec.rb
+++ spec/feature_spec/user_can_sign_in_out_and_delete_account_spec.rb
@@ -44,10 +44,26 @@ feature 'authenticated user may sign in', %Q(
 		click_button 'Sign in'
 
 		click_link 'Edit profile'
-		# save_and_open_page
 		click_button 'Delete my account'
 
 		expect(page).to have_content('Bye! Your account has been successfully cancelled. We hope to see you again soon.')
+	end
+
+	scenario 'user changes password' do
+		user = FactoryGirl.create(:user)
+		visit new_user_session_path
+		fill_in 'Email', with: user.email
+		fill_in 'Password', with: user.password
+		click_button 'Sign in'
+
+		visit root_path
+		click_link 'Edit profile'
+		fill_in 'Current password', with: user.password
+		fill_in 'Password', with: 'new_password'
+		fill_in 'Password confirmation', with: 'new_password'
+		click_button 'Update'
+
+		expect(page).to have_content("updated successfully")
 	end
 
 end
